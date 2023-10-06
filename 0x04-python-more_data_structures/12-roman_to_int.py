040a9ccafde8c4b9008370ef5672953be06fd27c
JESSE NII AYERTEY ADJAR <royaljejil2@yahoo.com>
2023-10-06 22:01:31 +0000

    Technical interview preparation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,9 +3,9 @@ def to_subtract(list_num):
     to_sub = 0
     max_list = max(list_num)
 
-    for n in list_num:
-        if max_list > n:
-            to_sub += n
+    for m in list_num:
+        if max_list > m:
+            to_sub += m
 
     return (max_list - to_sub)
 
@@ -16,7 +16,6 @@ def roman_to_int(roman_string):
 
     if not isinstance(roman_string, str):
         return 0
-
     rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     list_keys = list(rom_n.keys())
 
@@ -27,7 +26,7 @@ def roman_to_int(roman_string):
     for ch in roman_string:
         for r_num in list_keys:
             if r_num == ch:
-                 if rom_n.get(ch) <= last_rom:
+                if rom_n.get(ch) <= last_rom:
                     num += to_subtract(list_num)
                     list_num = [rom_n.get(ch)]
                 else:
