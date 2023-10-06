0f5e5bb252e84e48c3ae50722f432d09d6ad6b19
JESSE NII AYERTEY ADJAR <royaljejil2@yahoo.com>
2023-10-06 21:44:24 +0000

    Technical interview preparation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,9 +3,9 @@ def to_subtract(list_num):
     to_sub = 0
     max_list = max(list_num)
 
-    for i in list_num:
-        if max_list > i:
-            to_sub += i
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
 
     return (max_list - to_sub)
 
