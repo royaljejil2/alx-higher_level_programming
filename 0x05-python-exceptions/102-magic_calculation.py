bc9c0bd45188ac3acab9bdfa8c9a037263f08dd9
JESSE NII AYERTEY ADJAR <royaljejil2@yahoo.com>
2023-10-23 17:06:18 +0000

    ByteCode -> Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,9 +2,13 @@ def magic_calculation(a, b):
     result = 0
 
     for n in range(1, 3):
-        if n > a:
-            raise ValueError('Too far')
-        else:
-            result += a ** b / n
+        try:
+            if n > a:
+                raise ValueError('Too far')
+            else:
+                result += a ** b / n
+        except ValueError as e:
+            result = b + a
+            break
 
     return result
